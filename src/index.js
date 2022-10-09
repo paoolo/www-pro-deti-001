908fffa5f78dcdbb272e8828a76e8269a2f75882
Pavel Krbálek <pavel.krbalek@generaliceska.cz>
2022-10-09 16:28:43 +0200

    sound example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-//
-// Try changing this code while viewing the frontend in the browser to see it live reload.
-// See README.md for details on running this code.
-//
-
-console.log('Geometrické tvary v barvách Ceské Republiky');
+function beep()
+{
+    var audio = document.getElementById('beep');
+    audio.play();
+}
