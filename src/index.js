06cee6f34bb6d766b060771c09ec19a5f02c02b3
Pavel Krbálek <pavel.krbalek@generaliceska.cz>
2022-10-18 20:17:20 +0200

    work in progress automatically

--- src/index.js
+++ src/index.js
@@ -3,4 +3,38 @@
 // See README.md for details on running this code.
 //
 
-console.log('Hello world from Parcel!');
+let ball = document.getElementById("ball");
+
+document.addEventListener('keydown', function (e) {
+    if (e.which === 37) {
+        moveBallLeft()
+    }
+})
+
+function moveBallLeft() {
+    var leftNumbers = ball.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+
+    if (left > 0) {
+        ball.style.left = `${left - 1}px`
+    }
+}
+
+
+
+//var myVar = setInterval(spostaDiv, 90);
+var margin = 0;
+
+let l = window.screen.width;
+let w = 1300;
+
+/* function spostaDiv() {
+    console.log(w);
+    console.log(ball);
+    if (margin == w) {//
+        margin = 0 + "px";
+    } else {
+        ball.style.marginLeft = margin + "px";
+    }
+    margin += 10;
+} */
