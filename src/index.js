92b7a5264db819567a9e06fc6a93b090fa496231
Pavel Krbálek <pavel.krbalek@generaliceska.cz>
2022-11-02 07:49:57 +0100

    work in progress automatically

--- src/index.js
+++ src/index.js
@@ -3,4 +3,46 @@
 // See README.md for details on running this code.
 //
 
-console.log('Hello world from Parcel!');
+//source https://stackabuse.com/how-to-build-a-date-countdown-with-vanilla-javascript/
+
+let dayField = document.getElementById('day');
+let hourField = document.getElementById('hour');
+let minuteField = document.getElementById('minute');
+let secondField = document.getElementById('second');
+
+let interval;
+const eventDay = moment("2022-12-24");
+  
+// Convert to milisecond
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+  
+const countDownFn = () => {
+    const today = moment();
+    const timeSpan = eventDay.diff(today);
+  
+    if (timeSpan <= -today) {
+        console.log("Unfortunately we have past the event day");
+        clearInterval(interval);
+        return;
+    } else if (timeSpan <= 0) {
+        console.log("Today is the event day");
+        clearInterval(interval);
+        return;
+    } else {
+        const days = Math.floor(timeSpan / day);
+        const hours = Math.floor((timeSpan % day) / hour);
+        const minutes = Math.floor((timeSpan % hour) / minute);
+        const seconds = Math.floor((timeSpan % minute) / second);
+  
+        // Set results
+        dayField.innerHTML = days;
+        hourField.innerHTML = hours;
+        minuteField.innerHTML = minutes;
+        secondField.innerHTML = seconds;
+    }
+};
+  
+interval = setInterval(countDownFn, second);
